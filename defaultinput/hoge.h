c91ad16429688e682acdf7c5311f4c906a821982
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-10 18:22:58 -0800

    add new sample

---
+++ defaultinput/hoge.h
@@ -0,0 +1,6 @@
+
+int add(int i=1,int j=2){
+  return i+j;
+}
+
+
---
+++ defaultinput/test.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import example
+print("normal use")
+print(example.add(5, 1))
+print("default param")
+print(example.add())
